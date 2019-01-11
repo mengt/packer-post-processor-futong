f576b1588643b683a94a94c240586ed2c7410aa2
Ment_Niu <ment_niu@163.com>
2019-01-11 18:20:25 +0800

    Update artifact.go

--- artifact.go
+++ artifact.go
@@ -1,37 +1,62 @@
-package main
+package artifice
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 const BuilderId = "packer.post-processor.futong"
 
 type Artifact struct {
-	Url string
+	files []string
+}
+
+func NewArtifact(files []string) (*Artifact, error) {
+	artifact := &Artifact{}
+	for _, f := range files {
+		globfiles, err := filepath.Glob(f)
+		if err != nil {
+			return nil, err
+		}
+		for _, gf := range globfiles {
+			if _, err := os.Stat(gf); err != nil {
+				return nil, err
+			}
+			artifact.files = append(artifact.files, gf)
+		}
+	}
+	return artifact, nil
 }
 
 func (a *Artifact) BuilderId() string {
 	return BuilderId
 }
 
-func (*Artifact) Id() string {
+func (a *Artifact) Files() []string {
+	return a.files
+}
+
+func (a *Artifact) Id() string {
 	return ""
 }
 
-func (a *Artifact) Files() []string {
-	return nil
-}
-
 func (a *Artifact) String() string {
-	return fmt.Sprintf("URL of the template : %s", a.Url)
+	files := strings.Join(a.files, ", ")
+	return fmt.Sprintf("Created artifact from files: %s", files)
 }
 
-func (*Artifact) State(name string) interface{} {
+func (a *Artifact) State(name string) interface{} {
 	return nil
 }
 
 func (a *Artifact) Destroy() error {
-	// TODO
-	// Delete the template from Futong
+	for _, f := range a.files {
+		err := os.RemoveAll(f)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
